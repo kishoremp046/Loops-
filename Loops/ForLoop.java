25c17313fea26d342de0342713641f83391e3b9c
Kishore MP <155319917+kishoremp046@users.noreply.github.com>
2024-11-17 12:57:39 +0530

    Add files via upload

---
+++ Loops/ForLoop.java
@@ -0,0 +1,14 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--)
+		{
+			System.out.println(i);
+			
+		}
+		//2,4,6,8,10
+		for(int j=2;j<=10;j+=2)
+		{
+			System.out.println(j);
+		}
+	}
+}
---
+++ Loops/NestedFor.java
@@ -0,0 +1,14 @@
+public class NestedFor {
+   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=5;j<=6;j++)
+			{
+				System.out.println(i + "  "+ j);
+			}
+		}
+
+	} 
+}
